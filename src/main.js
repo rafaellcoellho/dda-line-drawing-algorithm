efb88eda260eceb8fa12f6d5c5a5888f1afc335f
rafaellcoellho <rafaellcoellho@gmail.com>
2020-11-02 22:12:00 -0300

    Implementa DDA

--- src/main.js
+++ src/main.js
@@ -2,11 +2,43 @@
 const HEIGHT = 700;
 const WIDTH = 700;
 
-const SIZE = 70;
+const SIZE = 35;
 
 let cols, rows;
+let raster;
 
-let raster = undefined;
+function generate_fragment (x, y) {
+  const xm = floor(x);
+  const ym = floor(y);
+  raster.putPixel(xm, ym);
+}
+
+function dda (x1, y1, x2, y2) {
+  let x = x1;
+  let y = y1;
+  const deltaX = x2 - x1;
+  const deltaY = y2 - y1;
+  const m = deltaY / deltaX;
+  const b = y - (m * x);
+
+  generate_fragment(x, y);
+
+  if (abs(deltaX) > abs(deltaY)) {
+    while (x < x2) {
+      x = x + 1;
+      y = (m * x) + b;
+  
+      generate_fragment(x, y);
+    }
+  } else {
+    while (y < y2) {
+      y = y + 1;
+      x = (y - b) / m;
+      
+      generate_fragment(x, y);
+    }
+  }
+}
 
 function setup () {
   createCanvas(HEIGHT, WIDTH);
@@ -15,7 +47,6 @@ function setup () {
   rows =  floor(WIDTH/SIZE);
 
   raster = new Raster(cols, rows);
-  raster.putPixel(3, 0);
 }
 
 function draw() {
